528b7fff7c3e33056b66f2cd2d7644b9d7bdb063
Magdalena Szczesna <magdalena.szczesna.ue@gmil.com>
2022-09-21 16:12:51 +0200

    change code

--- main.py
+++ main.py
@@ -8,19 +8,13 @@
 # w języku polskim, 
 # „687454786” – liczba też może być palindromem.
 
-def is_Palindrom(s1):
-    for character in s1:
-        if character.isalnum(): #sprawdzamy czy znaki we frazie są alfanumeryczne
-            #zwraca wartość true albo false
-            s2 = ""
-            s2 += character.lower() #zmieniamy wszystkie literki, 
-            n = len(s2) #zmienna pomocnicza przechowujaca dlugosc slowa
-            for i in range(n-1):
-                if s2[i] == s2[n-1-i]: #jezeli znak po przeciwnej stronie 
-                    #(w tej samej kolejnosci od konca) 
-                    return True; #jezeli nie napotkano problemow, 
-                    #zwroc true return(x)
+def is_palindrom(text):
+    text = text.lower()    # zamieniam na małe litery
+    text = [character for character in text if character.isalnum()]  # wybieram tylko litery i cyfry
+    # teraz jest to lista, ale to nie szkodzi    
+    return text == text[::-1]  # sprawdzam czy text to to samo co text wspak 
+    
 print("Program sprawdzajacy czy slowo jest palindromem")
 print("Podaj slowo")
-s1 = input() #pobieramy slowo
-print("Podane slowo " + ("jest " if(is_Palindrom(s1)) else "nie jest ") + "palindromem")
+text = input() #pobieramy slowo
+print("Podane slowo " + ("jest " if(is_palindrom(text)) else "nie jest ") + "palindromem")
